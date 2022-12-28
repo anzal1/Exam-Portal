873c1069971b41d7198daad14874239a19f55347
anzal1 <cofclan145@gmail.com>
2022-12-28 16:42:49 +0530

    code dump

---
+++ backend/public/precache-manifest.6e161570abbc54be38679b883cad376d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452b05635e3062a37849827a6d8af3a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0682d7471b2cd9087f",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "dd7af65ce3881e22a810",
+    "url": "/static/css/main.44c26bd8.chunk.css"
+  },
+  {
+    "revision": "bf0682d7471b2cd9087f",
+    "url": "/static/js/2.e0c31abe.chunk.js"
+  },
+  {
+    "revision": "dd7af65ce3881e22a810",
+    "url": "/static/js/main.24acd876.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e056993f08d6e0464578e28f8bfaaf91",
+    "url": "/static/media/homepage.e056993f.jpg"
+  },
+  {
+    "revision": "f6c74aeb503722fc25b719dc0360a18a",
+    "url": "/static/media/mainsss.f6c74aeb.png"
+  }
+]);
---
+++ backend/public/precache-manifest.da70e1e0d692b0438c540fd92ef82c8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ee37e6b55252b9e1dff5f8efe69b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0682d7471b2cd9087f",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "71ad2a70015e91f7fb56",
+    "url": "/static/css/main.44c26bd8.chunk.css"
+  },
+  {
+    "revision": "bf0682d7471b2cd9087f",
+    "url": "/static/js/2.e0c31abe.chunk.js"
+  },
+  {
+    "revision": "71ad2a70015e91f7fb56",
+    "url": "/static/js/main.612e9f1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e056993f08d6e0464578e28f8bfaaf91",
+    "url": "/static/media/homepage.e056993f.jpg"
+  },
+  {
+    "revision": "f6c74aeb503722fc25b719dc0360a18a",
+    "url": "/static/media/mainsss.f6c74aeb.png"
+  }
+]);
